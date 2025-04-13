f600e6d6d9a41de947f7115858a6c1f61824a01c
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2025-04-13 16:19:21 +0300

    Fix some CS and PHPStan issues

--- src/Window/Size/Size.php
+++ src/Window/Size/Size.php
@@ -10,12 +10,22 @@ final readonly class Size implements SizeInterface
 
     public function __construct(
         /**
-         * @var int<0, max>
+         * @var int<0, 2147483647>
          */
         public int $width = 0,
         /**
-         * @var int<0, max>
+         * @var int<0, 2147483647>
          */
         public int $height = 0,
-    ) {}
+    ) {
+        // @phpstan-ignore-next-line : DbC invariant
+        assert($width >= 0 && $width <= 2147483647, new \InvalidArgumentException(
+            message: 'Width CAN NOT be less than 0 or greater than 2147483647',
+        ));
+
+        // @phpstan-ignore-next-line : DbC invariant
+        assert($height >= 0 && $height <= 2147483647, new \InvalidArgumentException(
+            message: 'Height CAN NOT be less than 0 or greater than 2147483647',
+        ));
+    }
 }
