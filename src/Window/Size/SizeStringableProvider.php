cd17945e648c53aaa92693d13ef3a114c3c64209
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2025-04-13 16:26:38 +0300

    Add MacOS and Linux prebuilt binaries from CI

--- src/Window/Size/SizeStringableProvider.php
+++ src/Window/Size/SizeStringableProvider.php
@@ -6,11 +6,10 @@ namespace Serafim\Boson\Window\Size;
 
 /**
  * @phpstan-require-implements SizeInterface
+ * @mixin SizeInterface
  *
  * @internal This is an internal library trait, please do not use it in your code
  * @psalm-internal Serafim\Boson\Window
- *
- * @mixin SizeInterface
  */
 trait SizeStringableProvider
 {
