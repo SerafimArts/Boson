8f4f376d5f780ae6262f2e8e0d5089bf68b11731
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2025-04-14 19:27:14 +0300

    Fix NoDefaultWindowException message typo

--- src/Exception/NoDefaultWindowException.php
+++ src/Exception/NoDefaultWindowException.php
@@ -8,7 +8,7 @@ final class NoDefaultWindowException extends ApplicationException
 {
     public static function becauseNoDefaultWindow(?\Throwable $previous = null): self
     {
-        $message = 'There is no default window available,  perhaps it was removed (closed) earlier';
+        $message = 'There is no default window available, perhaps it was removed (closed) earlier';
 
         return new self($message, 0, $previous);
     }
